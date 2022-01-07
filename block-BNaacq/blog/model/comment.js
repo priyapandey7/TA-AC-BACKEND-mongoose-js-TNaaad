951d0f71f2d68b3763f346f59106da0a61f0f248
priya pandey <priyarock746@gmail.com>
2022-01-07 19:19:14 +0530

    Assignment

---
+++ block-BNaacq/blog/model/comment.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    content :String,
+    author :Schema.Types.ObjectId,
+    article : String
+
+}, {timestamps : true})
+
+var Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment;
+
+
+
+
+
+
+
+
+
+// Each comment can have fields:-
+
+// - content
+// - author -> ID of user
+// - article ->
+// - timestamps()
